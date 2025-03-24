bab5d4643f3c555b30d77f5e85cc922e3f5a2653
haruyuki9 <assassinsoul145@163.com>
2025-03-24 20:23:24 +0800

    Site updated: 2025-03-24 20:23:19

---
+++ collection/database.js
@@ -0,0 +1,100 @@
+const yrhm2023_database = [
+    {
+        href : "../images/yrhm2023/1.jpg", 
+        fancybox: "yrhm2023_1",
+        src : "../images/yrhm2023/1_index.jpg"
+    },
+    {
+        href : "../images/yrhm2023/2.jpg", 
+        fancybox: "yrhm2023_2",
+        src : "../images/yrhm2023/2_index.jpg"
+    },
+    {
+        href : "../images/yrhm2023/3.jpg", 
+        fancybox: "yrhm2023_3",
+        src : "../images/yrhm2023/3_index.jpg"
+    },
+    {
+        href : "../images/yrhm2023/4.jpg", 
+        fancybox: "yrhm2023_4",
+        src : "../images/yrhm2023/4_index.jpg"
+    },
+    {
+        href : "../images/yrhm2023/5.jpg", 
+        fancybox: "yrhm2023_5",
+        src : "../images/yrhm2023/5_index.jpg"
+    },
+    {
+        href : "../images/yrhm2023/6.jpg", 
+        fancybox: "yrhm2023_6",
+        src : "../images/yrhm2023/6_index.jpg"
+    },
+    {
+        href : "../images/yrhm2023/7.jpg", 
+        fancybox: "yrhm2023_7",
+        src : "../images/yrhm2023/7_index.jpg"
+    },
+    {
+        href : "../images/yrhm2023/8.jpg", 
+        fancybox: "yrhm2023_8",
+        src : "../images/yrhm2023/8_index.jpg"
+    },
+    {
+        href : "../images/yrhm2023/9.jpg", 
+        fancybox: "yrhm2023_9",
+        src : "../images/yrhm2023/9_index.jpg"
+    },
+    {
+        href : "../images/yrhm2023/10.jpg", 
+        fancybox: "yrhm2023_10",
+        src : "../images/yrhm2023/10_index.jpg"
+    },
+    {
+        href : "../images/yrhm2023/11.jpg", 
+        fancybox: "yrhm2023_11",
+        src : "../images/yrhm2023/11_index.jpg"
+    },
+    {
+        href : "../images/yrhm2023/12.jpg", 
+        fancybox: "yrhm2023_12",
+        src : "../images/yrhm2023/12_index.jpg"
+    }
+]
+const album = [
+    {
+        id : "2020",
+        src : "../images/collection/futarino.jpg"
+    },
+    {
+        id : "2021",
+        src : "../images/collection/love.jpg"
+    },
+    {
+        id : "2021",
+        src : "../images/collection/chimera.jpg"
+    },
+    {
+        id : "2022",
+        src : "../images/collection/comic_and_cosmic.jpg" 
+    },
+    {
+        id : "2023",
+        src : "../images/collection/meta.jpg"
+    },
+    {
+        id : "2024",
+        src : "../images/collection/blue_ending_nova.jpg"
+    },
+    {
+        id : "2024",
+        src : "../images/collection/heritage.jpg"
+    },
+    {
+        id : "2024",
+        src : "../images/collection/ReStart.jpg"
+    },
+    {
+        id : "2025_album",
+        src : "../images/collection/zero-go.jpg"
+    }
+]
